98997a8fabc5ab74320f925cc4bbb843ae9c09b0
KirillFisenko <79265463469@ya.ru>
2024-10-21 18:53:36 +0300

    Способы защиты от SQL инъекций

--- SQL_injection_test/Controllers/UserController.cs
+++ SQL_injection_test/Controllers/UserController.cs
@@ -19,5 +19,33 @@ namespace SQL_injection_test.Controllers
             var users = _context.Users.FromSqlRaw(query).ToList();
             return View(users);
         }
+
+        //// Использование подготовленных запросов (Prepared Statements)
+        //public IActionResult Index(string userName)
+        //{
+        //    var query = @$"PREPARE stmt FROM 'SELECT * FROM users WHERE user_name = ?';
+        //                   SET @user_name = '{userName}';
+        //                   EXECUTE stmt USING @user_name;
+        //                   DEALLOCATE PREPARE stmt;";
+        //    var users = _context.Users.FromSqlRaw(query).ToList();
+        //    return View(users);
+        //}
+
+        //// Использование хранимых процедур (Stored Procedures)
+        //public IActionResult Index(string userName)
+        //{
+        //    var query = $"CALL get_user_by_user_name('{userName}');";
+        //    var users = _context.Users.FromSqlRaw(query).ToList();
+        //    return View(users);
+        //}
+
+        //// Экранирование данных (Escaping Data)
+        //public IActionResult Index(string userName)
+        //{
+        //    var query = @$"SET @user_name = QUOTE('{userName}');
+        //                   SELECT * FROM users WHERE user_name = @user_name;";
+        //    var users = _context.Users.FromSqlRaw(query).ToList();
+        //    return View(users);
+        //}
     }
 }
